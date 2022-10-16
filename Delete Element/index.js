d4d285fe7d5c88d475227258aab0062e3e36279e
muzammil17sp <malikkath78@gmail.com>
2022-10-16 14:35:21 -0700

    add Delete Element and Practice chapter

---
+++ Delete Element/index.js
@@ -0,0 +1,8 @@
+const array = [10, 20, 30, 40, 50, 60];
+const position = 2;
+
+for (i = position; i < array.length; i++) {
+  array[i] = array[i + 1];
+}
+array.length = array.length -1
+console.log(array);
---
+++ Practice/index.js
@@ -0,0 +1,19 @@
+// const array = [10, 20, 30, 40, 50, 60];
+// const position = 3;
+// for (let i = array.length - 1; i >= 0; i--) {
+//   if (i >= position) {
+//     array[i + 1] = array[i];
+//     if (i === position) {
+//       array[i] = 200;
+//     }
+//   }
+// }
+// console.log(array);
+
+// const array = [10, 20, 30, 40, 50, 60];
+// const position = 1;
+// for (i = position; i < array.length - 1; i++) {
+//   array[i] = array[i + 1];
+// }
+// array.length = array.length - 1;
+// console.log(array);

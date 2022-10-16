0e635e4b2e74f28c1aded5a586cf866a4d8fe54e
muzammil17sp <malikkath78@gmail.com>
2022-10-16 11:54:39 -0700

    add array traversing and accessing chapter

---
+++ Traversing Accessing/index.js
@@ -0,0 +1,12 @@
+const array = [10, 20, 30, 40, 50, 60];
+
+// Traversing
+for (let i = 0; i < array.length; i++) {
+  console.log(array[i]);
+}
+
+// Accessing
+
+for (let i = 0; i < array.length; i++) {
+  console.log(`${i} index has property of ${array[i]}`);
+}
